08200d64e73b01b538a306fcd461d49c72b53ec6
rohith-baggam <rohith.baggam@aptagrim.com>
2024-08-15 09:00:47 +0530

    Encrypted Schema verfifcation is Done

--- data_sync/sender_utils/schema_verification.py
+++ data_sync/sender_utils/schema_verification.py
@@ -2,17 +2,16 @@
 from django.db import models
 from django.apps import apps
 
+# TODO Updated schema verification with new logic current one is not returning for manytomany
+
 
 def get_model_properties(model: models.Model) -> dict:
     """
     Retrieve and return all properties of a Django model, including fields, types, and attributes.
     """
-    print('get_model_properties', model)
     properties = {}
-
-    # Get all fields of the model
+    # ? Get all fields of the model
     fields = model._meta.get_fields()
-    print('fields', fields)
     for field in fields:
         field_info = {
             'type': type(field).__name__,
@@ -24,11 +23,11 @@ def get_model_properties(model: models.Model) -> dict:
             'db_index': getattr(field, 'db_index', None),
             'related_name': getattr(field, 'related_name', None),
             'related_model': getattr(field.remote_field, 'model', None) if hasattr(field, 'remote_field') else None,
-            # Get choices if available
+            # ? Get choices if available
             'choices': getattr(field, 'choices', None),
         }
 
-        # If the field has choices, get the choices and default choice
+        # ? If the field has choices, get the choices and default choice
         if field_info['choices']:
             field_info['choices'] = list(field_info['choices'])
             field_info['default_choice'] = field_info['default']
